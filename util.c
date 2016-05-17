a8c065eca01b65a5d3c8681e9d8347a6d345144a
dly <dlyding@example.com>
2016-05-17 20:19:30 +0800

    增加一些函数

--- util.c
+++ util.c
@@ -1,1 +1,91 @@
 #include "util.h"
+
+int read_conf(char *filename, conf_t *cf) {
+	char conf_buf[BUFLEN]; 
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        log_err("cannot open config file: %s", filename);
+        return DLY_CONF_ERROR;
+    }
+    char *delim_pos;
+
+    while (fgets(conf_buf, BUFLEN, fp)) {
+        delim_pos = strstr(conf_buf, DELIM);  
+        if (!delim_pos)
+            return DLY_CONF_ERROR;
+        
+        if (conf_buf[strlen(conf_buf) - 1] == '\n') {
+            conf_buf[strlen(conf_buf) - 1] = '\0';
+        }
+
+        if (strncmp("root", conf_buf, 4) == 0) {
+            strcpy(cf->root, delim_pos + 1);
+        }
+
+        if (strncmp("port", conf_buf, 4) == 0) {
+            cf->port = atoi(delim_pos + 1);     
+        }
+
+        if (strncmp("worker_num", conf_buf, 10) == 0) {
+            cf->worker_num = atoi(delim_pos + 1);
+        }
+    }
+
+    fclose(fp);
+    return DLY_OK;
+}
+
+int open_listenfd(int port) 
+{
+    if (port <= 0) {
+        port = 3000;
+    }
+
+    int listenfd, optval = 1;
+    struct sockaddr_in serveraddr;
+  
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	    return -1;
+ 
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+		   (const void *)&optval , sizeof(int)) < 0)
+	    return -1;
+
+    /* Listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+	    return -1;
+
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, LISTENQ) < 0)
+	    return -1;
+
+    return listenfd;
+}
+
+/*
+    make a socket non blocking. If a listen socket is a blocking socket, after it comes out from epoll and accepts the last connection, the next accpet will block, which is not what we want
+*/
+int make_socket_non_blocking(int fd) {
+    int flags, s;
+    flags = fcntl(fd, F_GETFL, 0);
+    if (flags == -1) {
+        log_err("fcntl");
+        return -1;
+    }
+
+    flags |= O_NONBLOCK;
+    s = fcntl(fd, F_SETFL, flags);
+    if (s == -1) {
+        log_err("fcntl");
+        return -1;
+    }
+    return 0;
+}
+
--- util.h
+++ util.h
@@ -1,12 +1,43 @@
 #ifndef __UTIL_H__
 #define __UTIL_H__
 
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <strings.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include "dbg.h"
+#include "coroutine.h"
+
 #define DLY_OK            0
 #define DLY_NULL_POINTER -1
 #define DLY_EAGAIN       -2
+#define DLY_CONF_ERROR   100
 
-#define CR '\r'
-#define LF '\n'
+#define LISTENQ     1024
+
+#define BUFLEN      1024
+#define ROOTLEN     256
+
+#define DELIM    "="
+#define CR       '\r'
+#define LF       '\n'
 #define CRLFCRLF "\r\n\r\n"
+
+typedef struct conf_s {
+    char root[ROOTLEN];
+    int port;
+    int worker_num;
+}conf_t;
+
+int read_conf(char *filename, conf_t *cf);    // 配置文件中不能有空格
+
+int open_listenfd(int port);
+int make_socket_non_blocking(int fd);
 
 #endif
