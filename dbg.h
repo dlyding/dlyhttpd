501246012de16bfe83989243aa3186d455742726
dly <dlyding@example.com>
2016-12-15 14:29:09 +0800

    增加日志中的进程号的显示

--- dbg.h
+++ dbg.h
@@ -5,6 +5,8 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 #define COL(x)  "\033[;" #x "m"  
 #define RED        COL(31)  
@@ -19,16 +21,16 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...) fprintf(stderr, YELLOW"DEBUG"DEFAULT" %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, YELLOW"[DEBUG] pid:%d,"DEFAULT" (%s:%d) " M "\n", getpid(), __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...) fprintf(stderr, RED"[ERROR]"DEFAULT" (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr, RED"[ERROR] pid:%d,"DEFAULT" (%s:%d: errno: %s) " M "\n", getpid(), __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_warn(M, ...) fprintf(stderr, MAGENTA"[WARN]"DEFAULT" (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+#define log_warn(M, ...) fprintf(stderr, MAGENTA"[WARN] pid:%d,"DEFAULT" (%s:%d: errno: %s) " M "\n", getpid(), __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-#define log_info(M, ...) fprintf(stderr, GREEN"[INFO]"DEFAULT" (%s:%d) " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#define log_info(M, ...) fprintf(stderr, GREEN"[INFO] pid:%d,"DEFAULT" (%s:%d) " M "\n", getpid(), __FILE__, __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); /* exit(1); */ }
 
